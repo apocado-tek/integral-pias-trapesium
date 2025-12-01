d982ca6092e2d7e75793744702e0bb75a10c4a7d
apocado-tek <liwetglek@gmail.com>
2025-12-01 11:49:18 +0700

    Modify input for function and remove error handling
    Updated the text input for function f(x) to have a default value of 0 and removed error handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,17 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-# Fungsi evaluator
 def f(x, func_str):
     return eval(func_str, {"x": x, "math": math})
 
 st.title("Kalkulator Integral Metode Trapesium")
 
-# Input dari user
-func_str = st.text_input("Masukkan fungsi f(x)")
+func_str = st.text_input("Masukkan fungsi f(x)", value = 0)
 a = st.number_input("Masukkan batas bawah a", value=0.0)
 b = st.number_input("Masukkan batas atas b", value=0.0)
 n = st.number_input("Masukkan jumlah pias n", value=0)
 
-# Tombol hitung
 if st.button("Hitung Integral"):
-    try:
         h = (b - a) / n
 
         f0 = f(a, func_str)
@@ -32,13 +28,9 @@ if st.button("Hitung Integral"):
 
         hasil = (h/2) * (f0 + 2*sum_mid + fn)
 
-        # Output ke UI
         st.write("### Hasil Perhitungan")
         st.write(f"Lebar pias (h) = **{h}**")
         st.write(f"f(a) = **{f0}**")
         st.write(f"f(b) = **{fn}**")
         st.write(f"Jumlah bagian tengah = **{sum_mid}**")
         st.success(f"Nilai integral ≈ **{hasil}**")
-
-    except Exception as e:
-        st.error(f"Terjadi error: {e}")
